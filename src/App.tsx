2d32501d5d9823b893de571551fd1f9657c978c0
Gabriel Brito Oliveira <gabrielbrito.dev@gmail.com>
2023-03-07 22:14:31 -0300

    feat: theme and global styles

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/themes/global";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hellow World</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
